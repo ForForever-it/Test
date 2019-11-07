3f68cff1211d0004cd16f218f0e4ae052d7e983e
zhangshuo <zhangshuo_0831@163.com>
2019-11-07 13:51:58 +0800

    提交嗯代码

--- src/cn/zzs/test/Test.java
+++ src/cn/zzs/test/Test.java
@@ -4,5 +4,6 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("你好，世界");
         System.out.println("lalala");
+        System.out.println("嗯");
     }
 }
