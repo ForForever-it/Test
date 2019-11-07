1731c550d859ef38bedc16c3b7eabbcf9ce6ff0b
zhangshuo <zhangshuo_0831@163.com>
2019-11-07 14:00:05 +0800

    提交sss代码

--- src/cn/zzs/test/Test.java
+++ src/cn/zzs/test/Test.java
@@ -4,8 +4,5 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("你好，世界");
         System.out.println("lalala");
-        System.out.println("嗯");
-        System.out.println("ssss");
-        System.out.println("ss");
     }
 }
